dfcdaa050b9cf3d973caccbb57e271b843ccb3ab
KaReeeeeeeeEM <s2kizzyjunior13052002@gmail.com>
2025-02-02 15:27:21 +0300

    feat: registered the service worker and made a PWA of the App

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,20 +7,14 @@ import {
   Button,
 } from "@heroui/react";
 
-export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
-};
-
 export default function NavBar() {
+  const speak = (message: string) => {
+    if (!message.trim()) return;
+    speechSynthesis.cancel(); // Stop ongoing speech
+    const utterance = new SpeechSynthesisUtterance(message);
+    utterance.lang = "en-US";
+    speechSynthesis.speak(utterance);
+  };
   return (
     <Navbar shouldHideOnScroll>
       <NavbarBrand>
@@ -28,7 +22,7 @@ export default function NavBar() {
       </NavbarBrand>
       <NavbarContent justify="end">
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" className="bg-yellow-300 rounded-md text-white font-bold" variant="flat">
+          <Button as={Link} color="primary" href="#" onPress={() => speak("Welcome to Necta Results Portal. Simply enter your registration number to get your results")} className="bg-yellow-300 rounded-md text-white font-bold" variant="flat">
            Translate
           </Button>
         </NavbarItem>
